07c4e35a89d7f08e67242b57943996def6cc8d4b
ZhengHongyi <111111@163.com>
2025-04-11 22:35:54 +0800

    4

--- src/main/java/com/jdjm/jdjmpicturebackend/service/PictureService.java
+++ src/main/java/com/jdjm/jdjmpicturebackend/service/PictureService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jdjm.jdjmpicturebackend.model.dto.picture.PictureQueryRequest;
 import com.jdjm.jdjmpicturebackend.model.dto.picture.PictureReviewRequest;
+import com.jdjm.jdjmpicturebackend.model.dto.picture.PictureUploadByBatchRequest;
 import com.jdjm.jdjmpicturebackend.model.dto.picture.PictureUploadRequest;
 import com.jdjm.jdjmpicturebackend.model.entity.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
@@ -27,6 +28,16 @@ public interface PictureService extends IService<Picture> {
     void validPicture(Picture picture);
 
     PictureVO uploadPicture (Object inputSource, PictureUploadRequest pictureUploadRequest, User loginUser);
+
+    /**
+     * 批量抓取和创建图片
+     *
+     * @param pictureUploadByBatchRequest
+     * @param loginUser
+     * @return 成功创建的图片数
+     */
+    Integer uploadPictureByBatch(PictureUploadByBatchRequest pictureUploadByBatchRequest,
+                                 User loginUser);
 
     /**
      * 获取查询对象
--- src/main/java/com/jdjm/jdjmpicturebackend/service/impl/PictureServiceImpl.java
+++ src/main/java/com/jdjm/jdjmpicturebackend/service/impl/PictureServiceImpl.java
@@ -17,6 +17,7 @@ import com.jdjm.jdjmpicturebackend.manager.upload.UrlPictureUpload;
 import com.jdjm.jdjmpicturebackend.model.dto.file.UploadPictureResult;
 import com.jdjm.jdjmpicturebackend.model.dto.picture.PictureQueryRequest;
 import com.jdjm.jdjmpicturebackend.model.dto.picture.PictureReviewRequest;
+import com.jdjm.jdjmpicturebackend.model.dto.picture.PictureUploadByBatchRequest;
 import com.jdjm.jdjmpicturebackend.model.dto.picture.PictureUploadRequest;
 import com.jdjm.jdjmpicturebackend.model.entity.Picture;
 import com.jdjm.jdjmpicturebackend.model.entity.User;
@@ -26,11 +27,17 @@ import com.jdjm.jdjmpicturebackend.model.vo.UserVO;
 import com.jdjm.jdjmpicturebackend.service.PictureService;
 import com.jdjm.jdjmpicturebackend.mapper.PictureMapper;
 import com.jdjm.jdjmpicturebackend.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -43,6 +50,7 @@ import java.util.stream.Collectors;
 * @createDate 2025-03-30 18:00:15
 */
 @Service
+@Slf4j
 public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
     implements PictureService{
 
@@ -104,7 +112,12 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
         //构造要入库的图片信息
         Picture picture = new Picture();
         picture.setUrl(uploadPictureResult.getUrl());
-        picture.setName(uploadPictureResult.getPicName());
+        //默认从图片解析服务中获取名称，如果外层传入了图片名称，则使用传入的名称
+        String picName = uploadPictureResult.getPicName();
+        if(pictureUploadRequest != null && StrUtil.isNotBlank(pictureUploadRequest.getPicName())){
+            picName = pictureUploadRequest.getPicName();
+        }
+        picture.setName(picName);
         picture.setPicSize(uploadPictureResult.getPicSize());
         picture.setPicWidth(uploadPictureResult.getPicWidth());
         picture.setPicHeight(uploadPictureResult.getPicHeight());
@@ -122,6 +135,67 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
         ThrowUtils.throwIf(!result,ErrorCode.OPERATION_ERROR,"图片上传失败，数据库操作失败");
         return PictureVO.objToVo(picture);
     }
+
+
+    @Override
+    public Integer uploadPictureByBatch(PictureUploadByBatchRequest pictureUploadByBatchRequest, User loginUser) {
+        // 校验参数
+        String searchText = pictureUploadByBatchRequest.getSearchText();
+        Integer count = pictureUploadByBatchRequest.getCount();
+        ThrowUtils.throwIf(count > 30, ErrorCode.PARAMS_ERROR, "最多 30 条");
+        //批量抓取的图片名称可能五花八门，这里需要统一一个图片名称前缀，默认为搜索关键词
+        String namePrefix = pictureUploadByBatchRequest.getNamePrefix();
+        if (StrUtil.isBlank(namePrefix)) {
+            namePrefix = searchText;
+        }
+        // 抓取内容
+        String fetchUrl = String.format("https://cn.bing.com/images/async?q=%s&mmasync=1", searchText);
+        Document document;
+        try {
+            document = Jsoup.connect(fetchUrl).get();
+        } catch (IOException e) {
+            log.error("获取页面失败", e);
+            throw new BusinessException(ErrorCode.OPERATION_ERROR, "获取页面失败");
+        }
+        // 解析内容
+        Element div = document.getElementsByClass("dgControl").first();
+        if (ObjUtil.isEmpty(div)) {
+            throw new BusinessException(ErrorCode.OPERATION_ERROR, "获取元素失败");
+        }
+        Elements imgElementList = div.select("img.mimg");
+        // 遍历元素，依次处理上传图片
+        int uploadCount = 0;
+        for (Element imgElement : imgElementList) {
+            String fileUrl = imgElement.attr("src");
+            if (StrUtil.isBlank(fileUrl)) {
+                log.info("当前链接为空，已跳过：{}", fileUrl);
+                continue;
+            }
+            // 处理图片的地址，防止转义或者和对象存储冲突的问题
+            // www.baidu.com?name=dog，应该只保留 www.baidu.com
+            int questionMarkIndex = fileUrl.indexOf("?");
+            if (questionMarkIndex > -1) {
+                fileUrl = fileUrl.substring(0, questionMarkIndex);
+            }
+            // 上传图片
+            PictureUploadRequest pictureUploadRequest = new PictureUploadRequest();
+            pictureUploadRequest.setFileUrl(fileUrl);
+            pictureUploadRequest.setPicName(namePrefix + (uploadCount + 1));
+            try {
+                PictureVO pictureVO = this.uploadPicture(fileUrl, pictureUploadRequest, loginUser);
+                log.info("图片上传成功，id = {}", pictureVO.getId());
+                uploadCount++;
+            } catch (Exception e) {
+                log.error("图片上传失败", e);
+                continue;
+            }
+            if (uploadCount >= count) {
+                break;
+            }
+        }
+        return uploadCount;
+    }
+
 
     @Override
     public QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest) {
